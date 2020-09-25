1106de0b5e7bd596f58a4c14bb2584a884b9aa62
Alexandre Mouton-Brady <alexandre.mouton-brady@airweb.fr>
2020-09-25 15:48:47 +0200

    :arrow_up: Update to yarn 2 + solid 0.20 + clean up deps

--- playground/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { render } from "solid-js/dom";
-
-const App = () => <h1>Hello world!!!</h1>;
-
-export const rootEl = document.getElementById("app");
-export const dispose = render(() => App, rootEl);

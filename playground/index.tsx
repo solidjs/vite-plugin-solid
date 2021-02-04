5b27b04a54078253d0f705847a63e61677e0c638
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2021-02-04 21:17:15 +0100

    :arrow_up: Update to solid 0.24

--- playground/index.tsx
+++ playground/index.tsx
@@ -1,37 +1,42 @@
-import { lazy } from 'solid-js';
+import { createSignal, lazy } from 'solid-js';
 import { render } from 'solid-js/web';
-import { Link } from 'solid-app-router';
-import { MetaProvider } from 'solid-meta';
-import { Router, Route, RouteDefinition } from 'solid-app-router';
+// import { Link } from 'solid-app-router';
+// import { MetaProvider } from 'solid-meta';
+// import { Router, Route, RouteDefinition } from 'solid-app-router';
 
 import Home from './pages';
 
-const routes: RouteDefinition[] = [
-  {
-    path: '/',
-    component: Home,
-  },
-  {
-    path: '/about',
-    component: lazy(() => import('./pages/about')),
-  },
-];
+// const routes: RouteDefinition[] = [
+//   {
+//     path: '/',
+//     component: Home,
+//   },
+//   {
+//     path: '/about',
+//     component: lazy(() => import('./pages/about')),
+//   },
+// ];
 
-const App = () => (
-  <>
-    <Link href="/">Home</Link>
+const App = () => {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <>
+      {/* <Link href="/">Home</Link>
     <Link href="/about">About!!!</Link>
-    <Route />
-  </>
-);
+    <Route /> */}
+      <button onClick={() => setCount(count() + 1)}>{count()}</button>
+    </>
+  );
+};
 
 const dispose = render(
   () => (
-    <Router routes={routes}>
-      <MetaProvider>
-        <App />
-      </MetaProvider>
-    </Router>
+    //<Router routes={routes}>
+    //  <MetaProvider>
+    <App />
+    //  </MetaProvider>
+    //</Router>
   ),
   document.getElementById('app'),
 );

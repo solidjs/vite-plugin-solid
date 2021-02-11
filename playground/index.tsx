418d494fc05077776a87d973e0828cb43741a935
Alexandre <amoutonbrady@gmail.com>
2021-02-11 19:24:00 +0100

    :white_check_mark: Added extra check in the playground

--- playground/index.tsx
+++ playground/index.tsx
@@ -1,30 +1,30 @@
 import { createSignal, lazy } from 'solid-js';
 import { render } from 'solid-js/web';
-// import { Link } from 'solid-app-router';
-// import { MetaProvider } from 'solid-meta';
-// import { Router, Route, RouteDefinition } from 'solid-app-router';
+import { Link } from 'solid-app-router';
+import { MetaProvider } from 'solid-meta';
+import { Router, Route, RouteDefinition } from 'solid-app-router';
 
 import Home from './pages';
 
-// const routes: RouteDefinition[] = [
-//   {
-//     path: '/',
-//     component: Home,
-//   },
-//   {
-//     path: '/about',
-//     component: lazy(() => import('./pages/about')),
-//   },
-// ];
+const routes: RouteDefinition[] = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: lazy(() => import('./pages/about')),
+  },
+];
 
 const App = () => {
   const [count, setCount] = createSignal(0);
 
   return (
     <>
-      {/* <Link href="/">Home</Link>
+      <Link href="/">Home</Link>
     <Link href="/about">About!!!</Link>
-    <Route /> */}
+    <Route />
       <button onClick={() => setCount(count() + 1)}>{count()}</button>
     </>
   );
@@ -32,11 +32,11 @@ const App = () => {
 
 const dispose = render(
   () => (
-    //<Router routes={routes}>
-    //  <MetaProvider>
+    <Router routes={routes}>
+     <MetaProvider>
     <App />
-    //  </MetaProvider>
-    //</Router>
+     </MetaProvider>
+    </Router>
   ),
   document.getElementById('app'),
 );

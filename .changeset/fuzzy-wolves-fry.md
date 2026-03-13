7f39675371f7baf259466fde4c73958021a765e7
Birk Skyum <74932975+birkskyum@users.noreply.github.com>
2026-03-13 17:21:24 +0100

    [main] Allow vite 8 in peerDeps (#242)

---
+++ .changeset/fuzzy-wolves-fry.md
@@ -0,0 +1,5 @@
+---
+'vite-plugin-solid': patch
+---
+
+allow vite >=3 in peer deps
---
+++ examples/vite-8/src/main.tsx
@@ -0,0 +1,8 @@
+import { render } from 'solid-js/web';
+import App from './App';
+
+const app = document.getElementById('app');
+
+if (app) {
+  render(() => <App />, app);
+}
